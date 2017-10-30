3ded07730fbb0d142f058b5fe2a180989f4e26a3
Halasi Tamás <trusted.tomato@gmail.com>
2017-10-30 19:34:54 +0100

    Add POST

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const urlLib = require('url');
+const querystring = require('querystring');
 
-const request = (url, options = {}) => new Promise((resolve, reject) => {
-	const urlParts = urlLib.parse(url);
-	const lib = urlParts.protocol === 'http:' ? require('http') : require('https');
+const request = (url, {method = 'GET', body, post, headers = {}, redirects = 5, stream = false} = {}) => new Promise((resolve, reject) => {
+	if(redirects-- < 0){
+		reject(new Error('Too much redirect!'));
+	}
+	const {host, path, protocol} = urlLib.parse(url);
+	const lib = protocol === 'http:' ? require('http') : require('https');
 
-	lib.get({
-		host: urlParts.host,
-		path: urlParts.path,
-		headers: Object.assign({}, options.headers)
-	}, resp => {
+	if(post){
+		method = 'POST';
+		body = post;
+	}
+	if(typeof body === 'object' && body !== null){
+		body = JSON.stringify(body);
+		if(!headers['Content-Type']){
+			headers['Content-Type'] = 'application/json';
+		}
+	}
+	if(body && !headers['Content-Length']){
+		headers['Content-Length'] = Buffer.byteLength(body);
+	}
+	console.log(method, body, headers);
+
+	
+	const req = lib.request({ host, path, headers, method }, resp => {		
 		if(resp.statusCode >= 300 && resp.statusCode < 400 && resp.headers.location){
-			request(urlLib.resolve(url, resp.headers.location), options).then(resolve).catch(reject);
+			request(urlLib.resolve(url, resp.headers.location), {method, body, headers, redirects: redirects - 1, stream}).then(resolve).catch(reject);
 			resp.destroy();
 		}else if(resp.statusCode === 200){
-			if(options.stream){
+			if(stream){
 				resolve(resp);
 			}else{
 				let body = '';
@@ -31,6 +47,11 @@ const request = (url, options = {}) => new Promise((resolve, reject) => {
 			reject(resp);
 		}
 	});
+
+	if(body){
+		req.write(body);
+	}
+	req.end();
 });
 
 module.exports = request;
